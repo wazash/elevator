94e46ab9fe900d76e6b209469059a0819b94e634
Mateusz Witek <51757549+wazash@users.noreply.github.com>
2022-05-13 11:18:21 +0200

    Added player rotation, updated VS Editor package

---
+++ My project/Assets/Scripts/Camera/MouseLook.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private float minRotation = -90f, maxRotation = 90f;
+
+    [SerializeField] private Transform body;
+
+    private float xRotation;
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        body.Rotate(Vector3.up, mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, minRotation, maxRotation);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+}
