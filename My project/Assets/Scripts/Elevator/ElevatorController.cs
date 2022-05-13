f234174cf5edf02c2efa73a193d707ac5fa47600
Mateusz Witek <51757549+wazash@users.noreply.github.com>
2022-05-13 17:32:06 +0200

    Added elevator controller script
    Elevator now move depends on clicked button on panel

---
+++ My project/Assets/Scripts/Elevator/ElevatorController.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour
+{
+    [SerializeField] private int currentFloor = 0;
+    [SerializeField] private Transform[] floorsPositions;
+
+    private int targetFloor;
+    private Rigidbody rb;
+
+    private bool isMoving = false;
+
+    public event Action OnFloorChanged;
+    public event Action OnStartMoving;
+    public event Action OnStopMoving;
+     
+    public int CurrentFloor { get { return currentFloor; } }
+
+    private void Awake()
+    {
+        ElevatorButton.OnButtonPressed += ElevatorButton_OnButtonPressed;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void ElevatorButton_OnButtonPressed(int floor)
+    {
+        targetFloor = floor;
+    }
+
+    private void FixedUpdate()
+    {
+        MoveElevator(targetFloor);
+    }
+
+    private void Update()
+    {
+        CheckCurrentFloor();
+    }
+
+    private void MoveElevator(int targetFloor)
+    {
+        if(targetFloor < floorsPositions.Length /*&& currentFloor != targetFloor*/)
+        {
+            Vector3 startPos = transform.position;
+            Vector3 endPos = new Vector3(transform.position.x, floorsPositions[targetFloor].position.y, transform.position.z);
+
+            if(transform.position == endPos)
+            {
+                if (isMoving)
+                {
+                    Debug.Log("StopMoving");
+                    OnStopMoving?.Invoke();
+                }
+                isMoving = false;
+                currentFloor = targetFloor;
+            }
+            else
+            {
+                if (!isMoving)
+                {
+                    Debug.Log("StartMoving");
+                    OnStartMoving?.Invoke();
+                }
+                isMoving = true;
+                transform.position = Vector3.MoveTowards(startPos, endPos, .05f);
+            }
+        }
+    }
+
+    private void CheckCurrentFloor()
+    {
+        for (int i = 0; i < floorsPositions.Length; i++)
+        {
+            float floorOffset = transform.position.y - floorsPositions[i].position.y;
+
+            if (Mathf.Abs(floorOffset) < 0.1f && currentFloor != i)
+            {
+                currentFloor = i;
+                Debug.Log($"Floor changed: {currentFloor}");
+                OnFloorChanged?.Invoke();
+            }
+        }
+
+    }
+}
--- My project/Assets/Scripts/Elevator/ElevatorPanelDisplay.cs
+++ My project/Assets/Scripts/Elevator/ElevatorPanelDisplay.cs
@@ -6,10 +6,16 @@ using TMPro;
 public class ElevatorPanelDisplay : MonoBehaviour
 {
     [SerializeField] private TMP_Text displayText;
+    [SerializeField] private ElevatorController elevator;
 
     private void Awake()
     {
         ElevatorButton.OnButtonPressed += ElevatorButton_OnButtonPressed;
+    }
+
+    private void Start()
+    {
+        UpdateDisplayText(elevator.CurrentFloor);
     }
 
     private void ElevatorButton_OnButtonPressed(int number)
