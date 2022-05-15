0017df96165846004c74a5eaba9946c52c0e20cd
Mateusz Witek <51757549+wazash@users.noreply.github.com>
2022-05-15 23:41:24 +0200

    Implemented elevator sounds effects
    Differents sounds while start, moving and stop. Uses OnStartMoving and OnStopMoving elevator controller events.

---
+++ My project/Assets/Speaker.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speaker : MonoBehaviour
+{
+    [SerializeField] private AudioClip startSFX, stopSFX, movingSFX;
+    [SerializeField] private AudioSource audioSource;
+
+    private ElevatorController elevatorController;
+
+    private bool isMovingPlaying = false;
+
+    private void Awake()
+    {
+        elevatorController = GetComponent<ElevatorController>();
+
+        elevatorController.OnStartMoving += ElevatorController_OnStartMoving;
+        elevatorController.OnStopMoving += ElevatorController_OnStopMoving;
+    }
+    private void OnDestroy()
+    {
+        elevatorController.OnStartMoving -= ElevatorController_OnStartMoving;
+        elevatorController.OnStopMoving -= ElevatorController_OnStopMoving;
+    }
+    private void ElevatorController_OnStartMoving()
+    {
+        PlayStartSFX();
+    }
+    private void ElevatorController_OnStopMoving()
+    {
+        StartCoroutine(PlayStopSFX());
+    }
+
+    private void PlayStartSFX()
+    {
+        audioSource.PlayOneShot(startSFX);
+
+        audioSource.clip = movingSFX;
+        audioSource.loop = true;
+        audioSource.PlayDelayed(startSFX.length);
+    }
+    private IEnumerator PlayStopSFX()
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(stopSFX);
+
+        yield return new WaitForSeconds(stopSFX.length);
+        StopAllCoroutines();
+    }
+}
