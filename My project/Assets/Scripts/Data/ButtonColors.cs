e8338dc3a2c82987f5e7f4244fd74a025d4eee6e
Mateusz Witek <51757549+wazash@users.noreply.github.com>
2022-05-16 13:38:38 +0200

    Added changing panel buttons color
    If button is active, change coor to green after press, if inactive change color to red

---
+++ My project/Assets/Scripts/Data/ButtonColors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class ButtonColors
+{
+    [SerializeField] private Color active;
+    public Color Active => active;
+
+    [SerializeField] private Color inactive;
+    public Color Inactive => inactive;
+
+    [SerializeField] private Color standard;
+    public Color Standard => standard;
+}
+
--- My project/Assets/Scripts/Elevator/ElevatorPanelButton.cs
+++ My project/Assets/Scripts/Elevator/ElevatorPanelButton.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class ElevatorPanelButton : MonoBehaviour, IInteractable
@@ -6,18 +7,43 @@ public class ElevatorPanelButton : MonoBehaviour, IInteractable
     [SerializeField] private int buttonNumber;
     [SerializeField] private bool isActive = true;
 
+    [SerializeField] private ButtonColors color;
+
+    private MeshRenderer meshRenderer;
+
     public static event Action<int> OnButtonPressed;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
 
     public void OnInteract()
     {
         if (isActive)
         {
+            ChangeButtonColor(color.Active);
             ButtonPressed(buttonNumber);
+        }
+        else
+        {
+            ChangeButtonColor(color.Inactive);
+            StartCoroutine(ChangeInactiveColorBack(1f));
         }
     }
 
     private void ButtonPressed(int number)
     {
         OnButtonPressed?.Invoke(number);
+    }
+
+    public void ChangeButtonColor(Color color)
+    {
+        meshRenderer.material.color = color;
+    }
+    private IEnumerator ChangeInactiveColorBack(float time)
+    {
+        yield return new WaitForSeconds(time);
+        ChangeButtonColor(color.Standard);
     }
 }
